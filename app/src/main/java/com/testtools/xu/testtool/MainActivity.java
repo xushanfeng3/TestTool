ffee09b31ae225e6145261a1316e2f099e59e62f
xushanfeng3 <red_sword@163.com>
2016-08-16 23:02:56 +0800

    add run method

--- app/src/main/java/com/testtools/xu/testtool/MainActivity.java
+++ app/src/main/java/com/testtools/xu/testtool/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import unitil.LogCollect;
 import unitil.LogCollector;
+import unitil.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,27 +17,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Button button1 = (Button)findViewById(R.id.start);
         final Button button2 = (Button)findViewById(R.id.end);
-        button2.setClickable(false);
-        final LogCollector logThread = LogCollector.getInstance(this);
+        button2.setEnabled(false);
+        final LogCollect logStart = new LogCollect();
         button1.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                button1.setText("Collecting.");
-                button1.setClickable(false);
-                logThread.launch();
-                button2.setClickable(true);
+                logStart.launch();
+                Logger.d("test123");
+                MainActivity.this.startLog(button1,button2);
                 button2.setOnClickListener(new Button.OnClickListener(){
                     @Override
                     public void onClick(View view) {
-                        logThread.cancel();
-                        button1.setClickable(true);
-                        button1.setText("startLog");
-                        button2.setClickable(false);
-                        button2.setText("endLog");
+                        logStart.cancel();
+                        MainActivity.this.endLog(button1,button2);
                     }
                 });
             }
         });
+
+    }
+    public void startLog(Button button1,Button button2){
+        button1.setText("collecting");
+        button1.setEnabled(false);
+        button2.setEnabled(true);
+    }
+    public void endLog(Button button1,Button button2){
+        button2.setEnabled(false);
+        button2.setText("stop");
+        button1.setEnabled(true);
     }
 
 }
